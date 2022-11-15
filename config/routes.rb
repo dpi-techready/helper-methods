219daf954f0eb7bc40fde66e6c6681ceb25e4c92
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-11-15 16:43:43 +0000

    Shorten routes and added some movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,38 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "movies", :action => "index" })
+  # get("/", { :controller => "movies", :action => "index" }) 
+
+
+  # remove curly brackets
+  # get("/", :controller => "movies", :action => "index" )
+
+  # remove hash rockets and move colons
+  # get("/", controller: "movies", action: "index" )
+
+  # remove parenthesis
+  # get "/", controller: "movies", action: "index"
+
+  # shorthand syntax
+  # get({"/" => "movies#index"})
+
+  # super shorthhand syntax
+  get "/" => "movies#index"
 
   # Routes for the Movie resource:
 
   # CREATE
-  post("/movies", { :controller => "movies", :action => "create" })
-  get("/movies/new", { :controller => "movies", :action => "new" })
+  post "/movies" => "movies#create"
+  get "/movies/new" => "movies#new"
           
   # READ
-  get("/movies", { :controller => "movies", :action => "index" })
-  get("/movies/:id", { :controller => "movies", :action => "show" })
+  get "/movies" => "movies#index"
+  get "/movies/:id" => "movies#show"
   
   # UPDATE
-  patch("/movies/:id", { :controller => "movies", :action => "update" })
-  get("/movies/:id/edit", { :controller => "movies", :action => "edit" })
+  patch "/movies/:id" => "movies#update"
+  get "/movies/:id/edit" => "movies#edit"
   
   # DELETE
-  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+  delete "/movies/:id" => "movies#destroy"
 
   #------------------------------
 end
